511cd1d91eeb988fcd9449d34588b6fc41e581bd
kylewh <kylevanho@gmail.com>
2017-04-22 04:54:56 +0800

    using redux-promise & redux-logger

--- src/configureStore.js
+++ src/configureStore.js
@@ -1,40 +1,9 @@
-import { createStore } from 'redux'
+import { createStore, applyMiddleware } from 'redux'
+import promise from 'redux-promise'
+import createLogger from 'redux-logger'
 import todoApp from './reducers'
-import { throttle } from 'lodash'
-
-const logger = (store) => (next) => {
-  if (!console.group) {
-    return next
-  }
-  // This means addLoggingToDispatch is a curried function
-  return (action) => {
-    console.group(action.type)
-    console.log('%c previous state: ', 'color: gray', store.getState())
-    console.log('%c action: ', 'color: blue', action)
-    const returnValue = next(action) // Dispatch: Hey I am here!
-    console.log('%c next state: ', 'color: green', store.getState())
-    console.groupEnd(action.type)
-    return returnValue
-  }
-}
-
-// Same trick as we did in addLoggingToDispatch
-const promise = (store) => (next) => (action) => {
-  if (typeof action.then === 'function') {
-    return action.then(rawDispatch)
-  }
-  return next(action)
-}
-
-const wrapDispatchWithMiddleWares = (store, middlewares) => {
-  // however we must reverse the executing order
-  middlewares.slice().reverse().forEach(middleware =>
-    store.dispatch = middleware(store)(store.dispatch)
-  )
-}
 
 const configureStore = () => {
-  const store = createStore(todoApp)
   // The purpose of the middlewares is to replace 
   // the single dispatch function with a chain of
   // composable dispatch functions which each can do something with an action.
@@ -44,10 +13,14 @@ const configureStore = () => {
   middlewares.push(promise)
 
   if (process.env.NODE_ENV !== 'production') {
-    middlewares.push(logger)
+    middlewares.push(createLogger())
   }
-  wrapDispatchWithMiddleWares(store, middlewares)
-  return store
+
+  // The last arguments of createStore called enhancer --optional
+  return store = createStore(
+    todoApp,
+    applyMiddleware(...middlewares)
+  )
 }
 
 export default configureStore
