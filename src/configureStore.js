54487353d85cbd9325da80be6fcfadb0c0cf05fc
kylewh <kylevanho@gmail.com>
2017-04-22 04:48:08 +0800

    implement middleware system

--- src/configureStore.js
+++ src/configureStore.js
@@ -2,48 +2,51 @@ import { createStore } from 'redux'
 import todoApp from './reducers'
 import { throttle } from 'lodash'
 
-const addLoggingToDispatch = (store) => {
-  const rawDispatch = store.dispatch
+const logger = (store) => (next) => {
   if (!console.group) {
-    return rawDispatch
+    return next
   }
-
+  // This means addLoggingToDispatch is a curried function
   return (action) => {
     console.group(action.type)
     console.log('%c previous state: ', 'color: gray', store.getState())
     console.log('%c action: ', 'color: blue', action)
-    const returnValue = rawDispatch(action) // Dispatch: Hey I am here!
-    console.log('%c next state: ','color: green', store.getState())
+    const returnValue = next(action) // Dispatch: Hey I am here!
+    console.log('%c next state: ', 'color: green', store.getState())
     console.groupEnd(action.type)
     return returnValue
   }
 }
 
 // Same trick as we did in addLoggingToDispatch
-const addPromiseSupportToDispatch = (store) => {
-  const rawDispatch = store.dispatch
-  return (action) => {
-    // Key Step: Recognize promise
-    if (typeof action.then === 'function') {
-      // rawDispatch will receive an action object as 
-      // fullfilled value which is returned by receiveTodos
-      // Then rawDispatch as the resolve function will be executed
-      // Hence we indirectly update the state
-      return action.then(rawDispatch)
-    }
-    return rawDispatch(action)
+const promise = (store) => (next) => (action) => {
+  if (typeof action.then === 'function') {
+    return action.then(rawDispatch)
   }
+  return next(action)
+}
+
+const wrapDispatchWithMiddleWares = (store, middlewares) => {
+  // however we must reverse the executing order
+  middlewares.slice().reverse().forEach(middleware =>
+    store.dispatch = middleware(store)(store.dispatch)
+  )
 }
 
 const configureStore = () => {
   const store = createStore(todoApp)
+  // The purpose of the middlewares is to replace 
+  // the single dispatch function with a chain of
+  // composable dispatch functions which each can do something with an action.
+  const middlewares = []
+  // specify the order in which the action propagates through the middlewares.
+  // Propagation order: recognize promise => log info 
+  middlewares.push(promise)
 
   if (process.env.NODE_ENV !== 'production') {
-    store.dispatch = addLoggingToDispatch(store)
+    middlewares.push(logger)
   }
-
-  store.dispatch = addPromiseSupportToDispatch(store)
-
+  wrapDispatchWithMiddleWares(store, middlewares)
   return store
 }
 
