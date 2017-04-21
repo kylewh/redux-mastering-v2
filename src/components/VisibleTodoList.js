c96b9e7574484662d15e27acd39f9b89b1c518fe
kylewh <kylevanho@gmail.com>
2017-04-22 02:30:39 +0800

    wrapping dispatch to log action

--- src/components/VisibleTodoList.js
+++ src/components/VisibleTodoList.js
@@ -3,22 +3,11 @@ import { toggleTodo } from '../actions'
 import { withRouter } from 'react-router-dom'
 import TodoList from './TodoList'
 import { getVisibleTodos } from '../reducers'
-/**
- * We noticed inside the getVisibleTodos,
- * the arguments was specified to todos inside the state
- * It's a problem once we changed the state structure.
- * We should extract this selection and put into reducer
- * we will create a selector to fullfill this selection
- * Remember : using selector is a common best-practice in react
- */
+
 const mapStateToProps = (state, { match }) => ({
   todos: getVisibleTodos(state, match.params.filter || 'all')
 })
 
-/**
- * Shortcut way
- * { propNames: actionCreator }
- */
 const VisibleTodoList = withRouter(connect(
   mapStateToProps,
   { onTodoClick: toggleTodo}
--- src/configureStore.js
+++ src/configureStore.js
@@ -3,13 +3,36 @@ import todoApp from './reducers'
 import { loadState, saveState } from './localStorage'
 import { throttle } from 'lodash'
 
+const addLoggingToDispatch = (store) => {
+  const rawDispatch = store.dispatch
+
+  if (!console.group) {
+    return rawDispatch
+  }
+  /**
+   * Here we do the wrapping
+   * As same as the original dispatch,
+   * It also receive an action as arguments,
+   * then return back the state.
+   */
+  return (action) => {
+    console.group(action.type)
+    console.log('%c previous state: ', 'color: gray', store.getState())
+    console.log('%c action: ', 'color: blue', action)
+    const returnValue = rawDispatch(action) // Dispatch: Hey I am here!
+    console.log('%c next state: ','color: green', store.getState())
+    console.groupEnd(action.type)
+    return returnValue
+  }
+}
+
 const configureStore = () => {
   const persistedState = loadState()
+  const store = createStore(todoApp, persistedState)
 
-  const store = createStore(
-    todoApp,
-    persistedState
-  )
+  if (process.env.NODE_ENV !== 'production') {
+    store.dispatch = addLoggingToDispatch(store)
+  }
 
   store.subscribe(throttle(() => {
     saveState({
